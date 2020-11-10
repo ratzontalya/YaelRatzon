177bbd59975ef08ebfb65863dbdf3fab12fa715c
ratzontalya <ratzontalya11@gmail.com>
2020-11-11 00:08:55 +0200

    talya AB menu

---
+++ Lawyer/BE/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public static class Enums
+    {
+        public enum AlphaAndBeta
+        {א,ב,ג,ד,ה,ו,ז,ח,ט,י,כ,ל,מ,נ,ס,ע,פ,צ,ק,ר,ש,ת}
+    }
+}
